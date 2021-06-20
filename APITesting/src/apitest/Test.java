bd9c345a09a6396f47a286c065d88e3a432e4310
MAYUR MALIK <MAYUR MALIK@LAPTOP-CMFMPBMS>
2021-06-20 19:14:58 +0530

    test commit

---
+++ APITesting/src/apitest/Test.java
@@ -0,0 +1,14 @@
+package apitest;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// rest assured majorly work on given when then
+		
+		
+		System.out.println("testdata");
+	}
+
+}
